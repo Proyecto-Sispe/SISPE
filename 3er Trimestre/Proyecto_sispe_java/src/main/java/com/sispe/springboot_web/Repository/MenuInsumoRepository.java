420850e48e30307a82efd63b43fde5459e0f25b2
vicman2007 <victormanuelsolanonino94@gmail.com>
2026-09-10 00:29:34 -0500

    refactorización: Actualizar las convenciones de nomenclatura de métodos en MenuInsumoRepository para lograr coherencia.

--- 3er Trimestre/Proyecto_sispe_java/src/main/java/com/sispe/springboot_web/Repository/MenuInsumoRepository.java
+++ 3er Trimestre/Proyecto_sispe_java/src/main/java/com/sispe/springboot_web/Repository/MenuInsumoRepository.java
@@ -2,10 +2,16 @@ package com.sispe.springboot_web.Repository;
 
 import com.sispe.springboot_web.Model.MenuInsumo;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface MenuInsumoRepository extends JpaRepository<MenuInsumo, Long> {
-    List<MenuInsumo> findByMenuId(Integer menuId);
-    Optional<MenuInsumo> findByMenuIdAndInsumoId(Integer menuId, Long insumoId);
+
+    List<MenuInsumo> findByMenu_Id(Integer menuId);
+
+    Optional<MenuInsumo> findByMenu_IdAndInsumo_Id(
+            Integer menuId,
+            Long insumoId
+    );
 }
